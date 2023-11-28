9ed15bbae7583e58a75eeeb0216dfc596771f94d
Michael Girgis <michael.girgis@live.ca>
2023-11-27 21:55:22 -0500

    script updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,54 @@
 var APIKey = "84c3cfc8c419d24716f07138ab250bb5";
-
-
+var forecastContainerEl = document.querySelector('#forecast-container');
+var cityNameEl = document.querySelector('#city');
 var searchFormEl = document.querySelector('#search-form');
 
-function handleSearchFormSubmit(event) {
+
+    
+
+var formSubmitHandler = function (event) {
     event.preventDefault();
 
-    var city = document.querySelector('#search-input').value;
-    console.log(city);
+    var city = document.querySelector('#city').value.trim();
+    // console.log(city);
+    // console.log(queryURL);
     
-    if (!searchInputVal) {
-        console.error('Please enter a valid city to search!');
+    if (city) {
+        getCurrent(city);
+        forecastContainerEl.textContent = '';
+        cityNameEl.value = '';
+    } else {
+        alert('Please enter a valid city name');
     }
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+};
 
+var getCurrent = function (city) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     console.log(queryURL);
 
-    location.assign(queryURL);
-}
+    fetch(queryURL)
+    .then(function (response) {
+        if (response.ok) {
+        response.json().then(function (data) {
+            displayCurrent(data, city);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to OpenWeather');
+      });
+  };
+    
+var displayCurrent
+    
+
+    
 
 
-searchFormEl.addEventListener('submit', handdleSearchFormSubmit);
+
+
+
+searchFormEl.addEventListener('submit', formSubmitHandler);
